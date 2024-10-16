85eaeec85db219351e671070ee31a5cac17e548f
Alessandro Riva <alex27riva@gmail.com>
2024-10-16 13:43:00 +0200

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2024 Alessandro Riva
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var Version = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the version of the program",
+	Long:  `Display the current version of this CLI tool.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("soc-cli version %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
