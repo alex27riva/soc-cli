936cab4c75e5c3f12e1c7ee9fe3d811b2b024d25
Alessandro Riva <alex27riva@gmail.com>
2024-10-17 20:59:01 +0200

    JSON output in version command

--- cmd/version.go
+++ cmd/version.go
@@ -7,21 +7,43 @@ See the LICENSE file for details.
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var Version = "dev"
+
+type verOutput struct {
+	Version string `json:"version"`
+}
+
+func displayVersion(asJSON bool) {
+	if asJSON {
+		jsonData, err := json.MarshalIndent(verOutput{Version: Version}, "", " ")
+		if err != nil {
+			log.Fatalf("Error marshalling JSON: %v", err)
+		}
+		fmt.Println(string(jsonData))
+
+	} else {
+		fmt.Printf("soc-cli version: %s\n", Version)
+	}
+
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the version of the program",
 	Long:  `Display the current version of this CLI tool.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("soc-cli version: %s\n", Version)
+		asJSON, _ := cmd.Flags().GetBool("json")
+		displayVersion(asJSON)
 	},
 }
 
 func init() {
+	versionCmd.Flags().Bool("json", false, "Output version in JSON format")
 	rootCmd.AddCommand(versionCmd)
 }
