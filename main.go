cd7437876383cb9b2b55155e7ed3d3dcb97a3d25
Alessandro Riva <alex27riva@gmail.com>
2024-10-18 19:23:42 +0200

    Moved config to separate package

--- main.go
+++ main.go
@@ -7,75 +7,17 @@ See the LICENSE file for details.
 package main
 
 import (
-	"fmt"
-	"github.com/spf13/viper"
 	"log"
-	"os"
-	"path/filepath"
 	"soc-cli/cmd"
+	"soc-cli/config"
 )
 
-const configTemplate = `api_keys:
-  urlscan:
-    api_key: your-urlscan-api-key
-
-  ipinfo:
-    api_key: your-ipinfo-api-key
-`
-
-func ensureConfigExists() error {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("could not find home directory: %v", err)
-	}
-
-	configDir := filepath.Join(home, ".config", "soc-cli")
-	configFile := filepath.Join(configDir, "config.yaml")
-
-	// Create the directory if it doesn't exist
-	if _, err := os.Stat(configDir); os.IsNotExist(err) {
-		if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
-			return fmt.Errorf("could not create config directory: %v", err)
-		}
-	}
-
-	// Create file with default config if doesn't exist
-	if _, err := os.Stat(configFile); os.IsNotExist(err) {
-		defaultConfig := []byte(configTemplate)
-		if err := os.WriteFile(configFile, defaultConfig, 0644); err != nil {
-			return fmt.Errorf("could not create config file: %v", err)
-		}
-		fmt.Println("A new configuration file was created at:", configFile)
-	}
-
-	return nil
-}
-
-func loadConfig() error {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("could not find home directory: %v", err)
-	}
-
-	configPath := filepath.Join(home, ".config", "soc-cli")
-
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(configPath)
-
-	if err := viper.ReadInConfig(); err != nil {
-		return fmt.Errorf("error reading config file: %v", err)
-	}
-
-	return nil
-}
-
 func main() {
-	if err := ensureConfigExists(); err != nil {
+	if err := config.EnsureConfigExists(); err != nil {
 		log.Fatalf("Error ensuring config exists: %v", err)
 	}
 
-	if err := loadConfig(); err != nil {
+	if err := config.LoadConfig(); err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 	cmd.Execute()
