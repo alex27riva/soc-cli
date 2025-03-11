b3ebe29e7a217a1deac7842ebdb7077e61c529f9
Alessandro Riva <alex27riva@gmail.com>
2025-03-11 20:22:34 +0100

    Remove debug in api.go

--- internal/util/api.go
+++ internal/util/api.go
@@ -11,20 +11,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
 )
-
-var debug bool
-
-func init() {
-	// Check if SOC_DEBUG is set and enable debug mode if it is
-	if val, exists := os.LookupEnv("SOC_DEBUG"); exists {
-		debug, _ = strconv.ParseBool(val)
-	}
-}
 
 func GetRaw(url string, headers map[string]string) (body []byte, err error) {
 	req, err := http.NewRequest("GET", url, nil)
@@ -51,7 +39,7 @@ func GetRaw(url string, headers map[string]string) (body []byte, err error) {
 	return body, nil
 }
 
-func HTTPGetJSON(url string, headers map[string]string, target interface{}) (sc int, err error) {
+func HTTPGetJSON(url string, headers map[string]string, target any) (sc int, err error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return 0, fmt.Errorf("error creating request: %w", err)
@@ -59,14 +47,6 @@ func HTTPGetJSON(url string, headers map[string]string, target interface{}) (sc 
 
 	for key, value := range headers {
 		req.Header.Set(key, value)
-	}
-
-	// Log request details if debug is enabled
-	if debug {
-		log.Printf("Making API request to URL: %s", url)
-		for key, value := range headers {
-			log.Printf("Header: %s = %s", key, value)
-		}
 	}
 
 	client := &http.Client{}
@@ -81,10 +61,6 @@ func HTTPGetJSON(url string, headers map[string]string, target interface{}) (sc 
 		return resp.StatusCode, fmt.Errorf("error reading response body: %w", err)
 	}
 
-	if debug {
-		log.Printf("Response body: %s", string(body))
-	}
-
 	err = json.Unmarshal(body, target)
 	if err != nil {
 		return resp.StatusCode, fmt.Errorf("error unmarshalling JSON response: %w", err)
@@ -93,7 +69,7 @@ func HTTPGetJSON(url string, headers map[string]string, target interface{}) (sc 
 	return resp.StatusCode, nil
 }
 
-func HTTPPostJSON(url string, headers map[string]string, body interface{}, target interface{}) error {
+func HTTPPostJSON(url string, headers map[string]string, body any, target interface{}) error {
 	// Marshal the body into JSON
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
@@ -112,15 +88,6 @@ func HTTPPostJSON(url string, headers map[string]string, body interface{}, targe
 	// Set Content-Type header to application/json
 	req.Header.Set("Content-Type", "application/json")
 
-	// Log request details if debug is enabled
-	if debug {
-		log.Printf("Making API request to URL: %s", url)
-		for key, value := range headers {
-			log.Printf("Header: %s = %s", key, value)
-		}
-		log.Printf("Request body: %s", string(jsonBody))
-	}
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -131,10 +98,6 @@ func HTTPPostJSON(url string, headers map[string]string, body interface{}, targe
 	bodyResp, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return fmt.Errorf("error reading response body: %w", err)
-	}
-
-	if debug {
-		log.Printf("Response body: %s", string(bodyResp))
 	}
 
 	err = json.Unmarshal(bodyResp, target)
