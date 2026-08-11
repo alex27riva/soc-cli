4f5579105e9aed3c66c3a16ef5fd7f7ce44ec83a
Alessandro Riva <alex27riva@gmail.com>
2026-08-11 14:55:31 +0200

    fix: missing main.go

---
+++ cmd/soc-cli/main.go
@@ -0,0 +1,15 @@
+/*
+Copyright © 2024 Alessandro Riva
+
+Licensed under the MIT License.
+See the LICENSE file for details.
+*/
+package main
+
+import (
+	"github.com/alex27riva/soc-cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
